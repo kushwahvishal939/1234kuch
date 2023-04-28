5fc2756b5bd86e3c4402c55bb32eb354976f82bf
Vishal kushwah <101382436+kushwahvishal939@users.noreply.github.com>
2023-04-28 13:07:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,9 @@ app.get("/api/products/:id", (req, res) => {
   res.send(data.products);
 });
 
-app.use(express.static(path.join(__dirname, "/../frontend/build")));
+app.use(express.static(path.join(__dirname, "/../frontend/public")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+  res.sendFile(path.join(`${__dirname}/../frontend/public/index.html`));
 });
 
 app.listen(config.PORT, () => {
