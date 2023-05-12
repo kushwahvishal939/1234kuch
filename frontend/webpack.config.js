f9189a881837515c9f2045ecfe73b91232e10bc0
Vishal kushwah <101382436+kushwahvishal939@users.noreply.github.com>
2023-05-12 14:36:16 +0530

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   devServer: {
     clientLogLevel: 'silent',
-    contentBase: './public',
+    contentBase: 'Pizza-Shop/frontend/public',
     historyApiFallback: true,
   },
 };
